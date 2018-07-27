ae67a1859d00b77408f8e93455302995fe5b06a4
Doug Coburn <doug@numetric.com>
2018-07-27 16:37:04 -0600

    static copy

---
+++ price_289_coupe.js
@@ -0,0 +1,550 @@
+function load()
+{
+window.status = "Kirkham Motorsports"
+}
+
+function count()
+{
+ 
+if (calc.finish[0].checked){
+   var wfilefinish = document.calc.finish[0].value = 0;
+ } else {
+   var wfilefinish = document.calc.finish.value = 0;
+ }
+
+if (calc.finish[1].checked){
+   var wbrushedaluminum = document.calc.finish[1].value = 19995;
+ } else {
+   var wbrushedaluminum = document.calc.finish[1].value = 0;
+ }
+
+if (calc.finish[2].checked) {
+   var wpabody = document.calc.finish[2].value = 19995;
+ } else {
+   var wpabody = document.calc.finish[2].value = 0;
+ }
+
+if (calc.pmstripes.checked) {
+   var wpmstripes = document.calc.pmstripes.value = 1995;
+ } else {
+   var wpmstripes = document.calc.pmstripes.value = 0;
+ }
+
+if (calc.ppstripes.checked) {
+   var wppstripes = document.calc.ppstripes.value = 395;
+ } else {
+   var wppstripes = document.calc.ppstripes.value = 0;
+ }
+
+
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+if (calc.frame[0].checked) {
+   var w3steelframe = document.calc.frame[0].value = 0;
+ } else {
+   var w3steelframe = document.calc.frame[0].value = 0;
+ } 
+
+if (calc.frame[1].checked) {
+   var w3ssframe = document.calc.frame[1].value = 9995;
+ } else {
+   var w3ssframe = document.calc.frame[1].value = 0;
+ }
+
+
+if (calc.rollbar.checked) {
+   var wrollbar = document.calc.rollbar.value = 0;
+ } else {
+   var wrollbar = document.calc.rollbar.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+if (calc.jackhooks.checked) {
+   var wjackhooks = document.calc.jackhooks.value = 0;
+ } else {
+   var wjackhooks = document.calc.jackhooks.value = 0;
+ }
+
+if (calc.brakescoops.checked) {
+   var wbrakescoops = document.calc.brakescoops.value = 0;
+ } else {
+   var wbrakescoops = document.calc.brakescoops.value = 0;
+ }
+
+if (calc.heatshields.checked) {
+   var wheatshields = document.calc.heatshields.value = 0;
+ } else {
+   var wheatshields = document.calc.heatshields.value = 0;
+ }
+
+
+if (calc.foam[0].checked) {
+   var wfoam = document.calc.foam[0].value = 0;
+ } else {
+   var wfoam = document.calc.foam[0].value = 0;
+ }
+
+if (calc.foam[1].checked) {
+   var winstfoam = document.calc.foam[1].value = 595;
+ } else {
+   var winstfoam = document.calc.foam[1].value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.carpet[0].checked) {
+   var wnyloncarpet = document.calc.carpet[0].value = 0;
+ } else {
+   var wnyloncarpet = document.calc.carpet[0].value = 0;
+ }
+
+if (calc.carpet[1].checked) {
+   var wnyloncarpetinst = document.calc.carpet[1].value = 0;
+ } else {
+   var wnyloncarpetinst = document.calc.carpet[1].value = 0;
+ }
+
+if (calc.carpet[2].checked) {
+   var wwoolcarpet = document.calc.carpet[2].value = 0;
+ } else {
+   var wwoolcarpet = document.calc.carpet[2].value = 0;
+ }
+
+if (calc.pedals.checked) {
+   var wpedals = document.calc.pedals.value = 695;
+ } else {
+   var wpedals = document.calc.pedals.value = 0;
+ }
+
+if (calc.pwdbulk.checked) {
+   var wpwdbulk = document.calc.pwdbulk.value = 0;
+ } else {
+   var wpwdbulk = document.calc.pwdbulk.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+if (calc.swheel[0].checked){
+   var wwheel15 = document.calc.swheel[0].value = 0;
+ } else {
+   var wwheel15 = document.calc.swheel.value = 0;
+ }
+
+if (calc.swheel[1].checked){
+   var wwheelleather = document.calc.swheel[1].value = 0;
+ } else {
+   var wwheelleather = document.calc.swheel[1].value = 0;
+ }
+
+if (calc.swheel[2].checked) {
+   var wwheel16 = document.calc.swheel[2].value = 0;
+ } else {
+   var wwheel16 = document.calc.swheel[2].value = 0;
+ }
+
+if (calc.leatherdash.checked) {
+   var wleatherdash = document.calc.leatherdash.value = 0;
+ } else {
+   var wleatherdash = document.calc.leatherdash.value = 0;
+ }
+
+if (calc.seatbelt.checked) {
+   var wseatbelt = document.calc.seatbelt.value = 125;
+ } else {
+   var wseatbelt = document.calc.seatbelt.value = 0;
+ }
+
+if (calc.fireext.checked) {
+   var wfireext = document.calc.fireext.value = 525;
+ } else {
+   var wfireext = document.calc.fireext.value = 0;
+ }
+
+if (calc.faird.checked) {
+   var wfaird = document.calc.faird.value = 0;
+ } else {
+   var wfaird = document.calc.faird.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.harness.checked) {
+   var wharness = document.calc.harness.value = 0;
+ } else {
+   var wharness = document.calc.harness.value = 0;
+ }
+
+if (calc.batteries.checked) {
+   var wbatteries = document.calc.batteries.value = 0;
+ } else {
+   var wbatteries = document.calc.batteries.value = 0;
+ }
+
+if (calc.batteryswitch.checked) {
+   var wbatteryswitch = document.calc.batteryswitch.value = 0;
+ } else {
+   var wbatteryswitch = document.calc.batteryswitch.value = 0;
+ }
+
+if (calc.gauges.checked) {
+   var wgauges = document.calc.gauges.value = 0;
+ } else {
+   var wgauges = document.calc.gauges.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.oilcooler.checked) {
+   var woilcooler = document.calc.oilcooler.value = 0;
+ } else {
+   var woilcooler = document.calc.oilcooler.value = 0;
+ }
+
+if (calc.oilassy.checked) {
+   var woilassy = document.calc.oilassy.value = 0;
+ } else {
+   var woilassy = document.calc.oilassy.value = 0;
+ }
+ 
+ 
+ if (calc.diffcooler.checked) {
+   var wdiffcooler = document.calc.diffcooler.value = 0;
+ } else {
+   var wdiffcooler = document.calc.diffcooler.value = 0;
+ }
+ 
+ 
+
+if (calc.windsormount.checked) {
+   var wwindsormount = document.calc.windsormount.value = 0;
+ } else {
+   var wwindsormount = document.calc.windsormount.value = 0;
+ }
+
+if (calc.fuelfilter.checked) {
+   var wfuelfilter = document.calc.fuelfilter.value = 0;
+ } else {
+   var wfuelfilter = document.calc.fuelfilter.value = 0;
+ }
+
+if (calc.steelshaft.checked) {
+   var wsteelshaft = document.calc.steelshaft.value = 0;
+ } else {
+   var wsteelshaft = document.calc.steelshaft.value = 0;
+ }
+
+if (calc.stainlesssteering.checked) {
+   var wstainlesssteering = document.calc.stainlesssteering.value = 125;
+ } else {
+   var wstainlesssteering = document.calc.stainlesssteering.value = 0;
+ }
+
+if (calc.fiacoolant.checked) {
+   var wfiacoolant = document.calc.fiacoolant.value = 0;
+ } else {
+   var wfiacoolant = document.calc.fiacoolant.value = 0;
+ }
+
+
+
+if (calc.altbracket.checked) {
+   var waltbracket = document.calc.altbracket.value = 95;
+ } else {
+   var waltbracket = document.calc.altbracket.value = 0;
+ }
+
+if (calc.oilvap.checked) {
+   var woilvap = document.calc.oilvap.value = 0;
+ } else {
+   var woilvap = document.calc.oilvap.value = 0;
+ }
+
+if (calc.trspring.checked) {
+   var wtrspring = document.calc.trspring.value = 0;
+ } else {
+   var wtrspring = document.calc.trspring.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.tremecmount.checked) {
+   var wtremecmount = document.calc.tremecmount.value = 0;
+ } else {
+   var wtremecmount = document.calc.tremecmount.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.alumdiff.checked) {
+   var walumdiff = document.calc.alumdiff.value = 0;
+ } else {
+   var walumdiff = document.calc.alumdiff.value = 0;
+ }
+
+if (calc.diffadapt.checked) {
+   var wdiffadapt = document.calc.diffadapt.value = 495;
+ } else {
+   var wdiffadapt = document.calc.diffadapt.value = 0;
+ }
+
+if (calc.diffhose.checked) {
+   var wdiffhose = document.calc.diffhose.value = 0;
+ } else {
+   var wdiffhose = document.calc.diffhose.value = 0;
+ }
+
+if (calc.driveshaft.checked) {
+   var wdriveshaft = document.calc.driveshaft.value = 0;
+ } else {
+   var wdriveshaft = document.calc.driveshaft.value = 0;
+ }
+
+if (calc.halfshafts.checked) {
+   var whalfshafts = document.calc.halfshafts.value = 0;
+ } else {
+   var whalfshafts = document.calc.halfshafts.value = 0;
+ }
+
+if (calc.safetyloop.checked) {
+   var wsafetyloop = document.calc.safetyloop.value = 0;
+ } else {
+   var wsafetyloop = document.calc.safetyloop.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+if (calc.shocks[0].checked) {
+   var wstandardshocks = document.calc.shocks[0].value = 0;
+ } else {
+   var wstandardshocks = document.calc.shocks[0].value = 0;
+ }
+
+if (calc.shocks[1].checked) {
+   var wpenskeshocks = document.calc.shocks[1].value = 0;
+ } else {
+   var wpenskeshocks = document.calc.shocks[1].value = 0;
+ }
+
+if (calc.shocks[2].checked) {
+   var wpenskeadj = document.calc.shocks[2].value = 2995;
+ } else {
+   var wpenskeadj = document.calc.shocks[2].value = 0;
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.brakes[0].checked) {
+   var wstdbrakes = document.calc.brakes[0].value = 0;
+ } else {
+   var wstdbrakes = document.calc.brakes[0].value = 0;
+ }
+  
+if (calc.brakes[1].checked) {
+   var wrotors = document.calc.brakes[1].value = 0;
+ } else {
+   var wrotors = document.calc.brakes[1].value = 0;
+ }
+ 
+ if (calc.girling.checked) {
+   var wgirling = document.calc.girling.value = 4995;
+ } else {
+   var wgirling = document.calc.girling.value = 0;
+ }
+ 
+ 
+if (calc.res.checked) {
+   var wres = document.calc.res.value = 0;
+ } else {
+   var wres = document.calc.res.value = 0;
+ }
+ 
+ if (calc.bleed.checked) {
+   var wbleed = document.calc.bleed.value = 125;
+ } else {
+   var wbleed = document.calc.bleed.value = 0;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+if (calc.tires.checked) {
+   var wtires = document.calc.tires.value = 0;
+ } else {
+   var wtires = document.calc.tires.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+if (calc.ti.checked) {
+   var wti = document.calc.ti.value = 2995;
+ } else {
+   var wti = document.calc.ti.value = 0;
+ }
+ 
+ if (calc.enginefia.checked) {
+   var wenginefia = document.calc.enginefia.value = 24995;
+ } else {
+   var wenginefia = document.calc.enginefia.value = 0;
+ }
+
+ 
+ if (calc.etrans.checked) {
+   var wetrans = document.calc.etrans.value = 2495;
+ } else {
+   var wetrans = document.calc.etrans.value = 0;
+ }
+ 
+ if (calc.ekit.checked) {
+   var wekit = document.calc.ekit.value = 2495;
+ } else {
+   var wekit = document.calc.ekit.value = 0;
+ }
+
+if (calc.installengine.checked) {
+   var winstallengine = document.calc.installengine.value = 9995;
+ } else {
+   var winstallengine = document.calc.installengine.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+ document.calc.pay.value = 175000 + wfilefinish + wbrushedaluminum + wpabody + wpmstripes + wppstripes + w3steelframe + w3ssframe + wrollbar + wjackhooks + wbrakescoops + wheatshields + wfoam + winstfoam + wnyloncarpet +wnyloncarpetinst + wwoolcarpet + wpedals
+ + wpwdbulk + wwheel15  + wwheelleather + wwheel16 + wleatherdash + wseatbelt + wfireext + wfaird + wharness + wbatteries + wbatteryswitch + wgauges + woilcooler + woilassy + wdiffcooler + wwindsormount + wfuelfilter + wstainlesssteering + wtremecmount + walumdiff + wdiffadapt + wdiffhose + wdriveshaft + whalfshafts + wsafetyloop + wstandardshocks + wpenskeshocks + wpenskeadj + wstdbrakes + wrotors + wgirling + wres + wtires + wbleed + wsteelshaft + wfiacoolant + waltbracket + woilvap + wtrspring + wti + wenginefia + wetrans + wekit + winstallengine;
+}
+
+
---
+++ price_289_fia4_script.js
@@ -0,0 +1,700 @@
+function load()
+{
+window.status = "Kirkham Motorsports"
+}
+
+function count()
+{
+ 
+if (calc.body[0].checked){
+   var wfilefinish = document.calc.body[0].value = 0;
+ } else {
+   var wfilefinish = document.calc.body.value = 0;
+ }
+
+if (calc.body[1].checked){
+   var wcopperbody = document.calc.body[1].value = 39995;
+ } else {
+   var wcopperbody = document.calc.body[1].value = 0;
+ }
+
+if (calc.body[2].checked){
+   var wbronzebody = document.calc.body[2].value = 49995;
+ } else {
+   var wbronzebody = document.calc.body[2].value = 0;
+ }
+
+if (calc.finish[0].checked){
+   var wbrushedaluminum = document.calc.finish[0].value = 14995;
+ } else {
+   var wbrushedaluminum = document.calc.finish[0].value = 0;
+ }
+
+if (calc.finish[1].checked) {
+   var wpabody = document.calc.finish[1].value = 14995;
+ } else {
+   var wpabody = document.calc.finish[1].value = 0;
+ }
+
+if (calc.pmstripes.checked) {
+   var wpmstripes = document.calc.pmstripes.value = 1995;
+ } else {
+   var wpmstripes = document.calc.pmstripes.value = 0;
+ }
+
+if (calc.ppstripes.checked) {
+   var wppstripes = document.calc.ppstripes.value = 395;
+ } else {
+   var wppstripes = document.calc.ppstripes.value = 0;
+ }
+
+if (calc.meatball.checked) {
+   var wmeatball = document.calc.meatball.value = 4995;
+ } else {
+   var wmeatball = document.calc.meatball.value = 0;
+ }
+
+if (calc.lemanstop.checked) {
+   var wlemanstop = document.calc.lemanstop.value = 14995;
+ } else {
+   var wlemanstop = document.calc.lemanstop.value = 0;
+ } 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+if (calc.frame[0].checked) {
+   var w3steelframe = document.calc.frame[0].value = 0;
+ } else {
+   var w3steelframe = document.calc.frame[0].value = 0;
+ } 
+
+if (calc.frame[1].checked) {
+   var w3ssframe = document.calc.frame[1].value = 4995;
+ } else {
+   var w3ssframe = document.calc.frame[1].value = 0;
+ }
+
+
+if (calc.rollbar[0].checked) {
+   var wrollbar = document.calc.rollbar[0].value = 0;
+ } else {
+   var wrollbar = document.calc.rollbar.value = 0;
+ }
+if (calc.rollbar[1].checked) {
+   var wssrollbar = document.calc.rollbar[1].value = 495;
+ } else {
+   var wssrollbar = document.calc.rollbar[1].value = 0;
+ }
+if (calc.rollbar[2].checked) {
+   var w2xrollbar = document.calc.rollbar[2].value = 1995;
+ } else {
+   var w2xrollbar = document.calc.rollbar[2].value = 0;
+ }
+
+if (calc.rhd.checked) {
+   var wrhd = document.calc.rhd.value = 1495;
+ } else {
+   var wrhd = document.calc.rhd.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+if (calc.jackhooks.checked) {
+   var wjackhooks = document.calc.jackhooks.value = 0;
+ } else {
+   var wjackhooks = document.calc.jackhooks.value = 0;
+ }
+
+ 
+if (calc.brakescoops.checked) {
+   var wbrakescoops = document.calc.brakescoops.value = 995;
+ } else {
+   var wbrakescoops = document.calc.brakescoops.value = 0;
+ }
+
+if (calc.heatshields.checked) {
+   var wheatshields = document.calc.heatshields.value = 495;
+ } else {
+   var wheatshields = document.calc.heatshields.value = 0;
+ }
+
+
+if (calc.foam[0].checked) {
+   var wnofoam = document.calc.foam[0].value = 0;
+ } else {
+   var wnofoam = document.calc.foam[0].value = 0;
+ }
+
+if (calc.foam[1].checked) {
+   var wfoam = document.calc.foam[1].value = 0;
+ } else {
+   var wfoam = document.calc.foam[1].value = 0;
+ }
+
+if (calc.foam[2].checked) {
+   var winstfoam = document.calc.foam[2].value = 595;
+ } else {
+   var winstfoam = document.calc.foam[2].value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.carpet[0].checked) {
+   var wnyloncarpet = document.calc.carpet[0].value = 0;
+ } else {
+   var wnyloncarpet = document.calc.carpet[0].value = 0;
+ }
+
+if (calc.carpet[1].checked) {
+   var wnyloncarpetinst = document.calc.carpet[1].value = 0;
+ } else {
+   var wnyloncarpetinst = document.calc.carpet[1].value = 0;
+ }
+
+if (calc.carpet[2].checked) {
+   var wwoolcarpet = document.calc.carpet[2].value = 0;
+ } else {
+   var wwoolcarpet = document.calc.carpet[2].value = 0;
+ }
+
+if (calc.carpet[3].checked) {
+   var wwoolcarpetinst = document.calc.carpet[3].value = 695;
+ } else {
+   var wwoolcarpetinst = document.calc.carpet[3].value = 0;
+ }
+
+if (calc.seattracks.checked) {
+   var wseattracks = document.calc.seattracks.value = 0;
+ } else {
+   var wseattracks = document.calc.seattracks.value = 0;
+ }
+
+if (calc.sunvisor.checked) {
+   var wsunvisor = document.calc.sunvisor.value = 995;
+ } else {
+   var wsunvisor = document.calc.sunvisor.value = 0;
+ }
+
+if (calc.windwings.checked) {
+   var wwindwings = document.calc.windwings.value = 995;
+ } else {
+   var wwindwings = document.calc.windwings.value = 0;
+ }
+
+if (calc.pedals.checked) {
+   var wpedals = document.calc.pedals.value = 695;
+ } else {
+   var wpedals = document.calc.pedals.value = 0;
+ }
+
+if (calc.pwdbulk.checked) {
+   var wpwdbulk = document.calc.pwdbulk.value = 495;
+ } else {
+   var wpwdbulk = document.calc.pwdbulk.value = 0;
+ }
+
+
+
+
+ 
+if (calc.swheel[0].checked){
+   var wwheel15 = document.calc.swheel[0].value = 0;
+ } else {
+   var wwheel15 = document.calc.swheel.value = 0;
+ }
+
+if (calc.swheel[1].checked){
+   var wwheelleather = document.calc.swheel[1].value = 0;
+ } else {
+   var wwheelleather = document.calc.swheel[1].value = 0;
+ }
+
+if (calc.swheel[2].checked) {
+   var wwheel16 = document.calc.swheel[2].value = 0;
+ } else {
+   var wwheel16 = document.calc.swheel[2].value = 0;
+ }
+
+if (calc.leatherdash.checked) {
+   var wleatherdash = document.calc.leatherdash.value = 0;
+ } else {
+   var wleatherdash = document.calc.leatherdash.value = 0;
+ }
+
+if (calc.seatbelt.checked) {
+   var wseatbelt = document.calc.seatbelt.value = 125;
+ } else {
+   var wseatbelt = document.calc.seatbelt.value = 0;
+ }
+
+if (calc.fireext.checked) {
+   var wfireext = document.calc.fireext.value = 525;
+ } else {
+   var wfireext = document.calc.fireext.value = 0;
+ }
+
+if (calc.faird.checked) {
+   var wfaird = document.calc.faird.value = 0;
+ } else {
+   var wfaird = document.calc.faird.value = 0;
+ }
+
+if (calc.fairp.checked) {
+   var wfairp = document.calc.fairp.value = 495;
+ } else {
+   var wfairp = document.calc.fairp.value = 0;
+ }
+
+if (calc.dropbox.checked) {
+   var wdropbox = document.calc.dropbox.value = 1495;
+ } else {
+   var wdropbox = document.calc.dropbox.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.harness.checked) {
+   var wharness = document.calc.harness.value = 0;
+ } else {
+   var wharness = document.calc.harness.value = 0;
+ }
+
+if (calc.batteries.checked) {
+   var wbatteries = document.calc.batteries.value = 0;
+ } else {
+   var wbatteries = document.calc.batteries.value = 0;
+ }
+
+if (calc.batteryswitch.checked) {
+   var wbatteryswitch = document.calc.batteryswitch.value = 0;
+ } else {
+   var wbatteryswitch = document.calc.batteryswitch.value = 0;
+ }
+
+if (calc.gauges.checked) {
+   var wgauges = document.calc.gauges.value = 0;
+ } else {
+   var wgauges = document.calc.gauges.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.oilcooler.checked) {
+   var woilcooler = document.calc.oilcooler.value = 0;
+ } else {
+   var woilcooler = document.calc.oilcooler.value = 0;
+ }
+
+if (calc.oilassy.checked) {
+   var woilassy = document.calc.oilassy.value = 0;
+ } else {
+   var woilassy = document.calc.oilassy.value = 0;
+ }
+
+if (calc.windsormount.checked) {
+   var wwindsormount = document.calc.windsormount.value = 0;
+ } else {
+   var wwindsormount = document.calc.windsormount.value = 0;
+ }
+
+if (calc.fuelfilter.checked) {
+   var wfuelfilter = document.calc.fuelfilter.value = 0;
+ } else {
+   var wfuelfilter = document.calc.fuelfilter.value = 0;
+ }
+
+if (calc.steering[0].checked) {
+   var wsteelshaft = document.calc.steering[0].value = 0;
+ } else {
+   var wsteelshaft = document.calc.steering.value = 0;
+ }
+
+if (calc.steering[1].checked) {
+   var wstainlesssteering = document.calc.steering[1].value = 125;
+ } else {
+   var wstainlesssteering = document.calc.steering[1].value = 0;
+ }
+
+if (calc.coolant[0].checked) {
+   var wfiacoolant = document.calc.coolant[0].value = 495;
+ } else {
+   var wfiacoolant = document.calc.coolant.value = 0;
+ }
+
+if (calc.coolant[1].checked) {
+   var wbilletcoolant = document.calc.coolant[1].value = 495;
+ } else {
+   var wbilletcoolant = document.calc.coolant[1].value = 0;
+ }
+
+
+if (calc.altbracket.checked) {
+   var waltbracket = document.calc.altbracket.value = 95;
+ } else {
+   var waltbracket = document.calc.altbracket.value = 0;
+ }
+
+if (calc.oilvap.checked) {
+   var woilvap = document.calc.oilvap.value = 495;
+ } else {
+   var woilvap = document.calc.oilvap.value = 0;
+ }
+
+if (calc.trspring.checked) {
+   var wtrspring = document.calc.trspring.value = 495;
+ } else {
+   var wtrspring = document.calc.trspring.value = 0;
+ }
+
+if (calc.pipes[0].checked){
+   var wsidesteel = document.calc.pipes[0].value = 0;
+ } else {
+   var wsidesteel = document.calc.pipes.value = 0;
+ }
+ 
+ if (calc.pipes[1].checked){
+   var wsidestainless = document.calc.pipes[1].value = 2495;
+ } else {
+   var wsidestainless = document.calc.pipes[1].value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.tremecmount.checked) {
+   var wtremecmount = document.calc.tremecmount.value = 0;
+ } else {
+   var wtremecmount = document.calc.tremecmount.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.alumdiff.checked) {
+   var walumdiff = document.calc.alumdiff.value = 0;
+ } else {
+   var walumdiff = document.calc.alumdiff.value = 0;
+ }
+
+if (calc.diffhose.checked) {
+   var wdiffhose = document.calc.diffhose.value = 0;
+ } else {
+   var wdiffhose = document.calc.diffhose.value = 0;
+ }
+
+if (calc.diffcooler.checked) {
+   var wdiffcooler = document.calc.diffcooler.value = 1995;
+ } else {
+   var wdiffcooler = document.calc.diffcooler.value = 0;
+ }
+
+if (calc.driveshaft.checked) {
+   var wdriveshaft = document.calc.driveshaft.value = 0;
+ } else {
+   var wdriveshaft = document.calc.driveshaft.value = 0;
+ }
+
+if (calc.halfshafts[0].checked) {
+   var whalfshafts = document.calc.halfshafts[0].value = 0;
+ } else {
+   var whalfshafts = document.calc.halfshafts.value = 0;
+ }
+ 
+ if (calc.halfshafts[1].checked) {
+   var wdiffadapt = document.calc.halfshafts[1].value = 495;
+ } else {
+   var wdiffadapt = document.calc.halfshafts[1].value = 0;
+ }
+ 
+
+if (calc.safetyloop.checked) {
+   var wsafetyloop = document.calc.safetyloop.value = 95;
+ } else {
+   var wsafetyloop = document.calc.safetyloop.value = 0;
+ }
+
+
+
+
+
+
+
+
+if (calc.suspension[0].checked) {
+   var wbilletsusp = document.calc.suspension[0].value = 0;
+ } else {
+   var wbilletsusp = document.calc.suspension[0].value = 0;
+ }
+
+
+if (calc.suspension[1].checked) {
+   var wstandardshocks = document.calc.suspension[1].value = 9995;
+ } else {
+   var wstandardshocks = document.calc.suspension[1].value = 0;
+ }
+
+if (calc.shocks[0].checked) {
+   var wpenskeshocks = document.calc.shocks[0].value = 0;
+ } else {
+   var wpenskeshocks = document.calc.shocks[0].value = 0;
+ }
+
+if (calc.shocks[1].checked) {
+   var wpenskeadj = document.calc.shocks[1].value = 2995;
+ } else {
+   var wpenskeadj = document.calc.shocks[1].value = 0;
+ }
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.brakes[0].checked) {
+   var wstdbrakes = document.calc.brakes[0].value = 0;
+ } else {
+   var wstdbrakes = document.calc.brakes[0].value = 0;
+ }
+  
+if (calc.brakes[1].checked) {
+   var wgirling = document.calc.brakes[1].value = 4995;
+ } else {
+   var wgirling = document.calc.brakes[1].value = 0;
+ }
+ 
+ if (calc.sslines.checked) {
+   var wsslines = document.calc.sslines.value = 0;
+ } else {
+   var wsslines = document.calc.sslines.value = 0;
+ }
+ 
+if (calc.res.checked) {
+   var wres = document.calc.res.value = 0;
+ } else {
+   var wres = document.calc.res.value = 0;
+ }
+ 
+ if (calc.bleed.checked) {
+   var wbleed = document.calc.bleed.value = 125;
+ } else {
+   var wbleed = document.calc.bleed.value = 0;
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+
+ 
+ 
+if (calc.tires.checked) {
+   var wtires = document.calc.tires.value = 0;
+ } else {
+   var wtires = document.calc.tires.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.ti.checked) {
+   var wti = document.calc.ti.value = 2995;
+ } else {
+   var wti = document.calc.ti.value = 0;
+ }
+ 
+ if (calc.enginefia.checked) {
+   var wenginefia = document.calc.enginefia.value = 24995;
+ } else {
+   var wenginefia = document.calc.enginefia.value = 0;
+ }
+
+ 
+ if (calc.etrans.checked) {
+   var wetrans = document.calc.etrans.value = 2495;
+ } else {
+   var wetrans = document.calc.etrans.value = 0;
+ }
+ 
+ if (calc.ekit.checked) {
+   var wekit = document.calc.ekit.value = 2495;
+ } else {
+   var wekit = document.calc.ekit.value = 0;
+ }
+
+if (calc.installengine.checked) {
+   var winstallengine = document.calc.installengine.value = 9995;
+ } else {
+   var winstallengine = document.calc.installengine.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if (calc.bronzetpan.checked) {
+   var wbronzetpan = document.calc.bronzetpan.value = 995;
+ } else {
+   var wbronzetpan = document.calc.bronzetpan.value = 0;
+ }
+ 
+ if (calc.glare.checked) {
+   var wglare = document.calc.glare.value = 355;
+ } else {
+   var wglare = document.calc.glare.value = 0;
+ }
+
+ 
+ if (calc.bronzebling.checked) {
+   var wbronzebling = document.calc.bronzebling.value = 3595;
+ } else {
+   var wbronzebling = document.calc.bronzebling.value = 0;
+ }
+ 
+ if (calc.trumpets.checked) {
+   var wtrumpets = document.calc.trumpets.value = 995;
+ } else {
+   var wtrumpets = document.calc.trumpets.value = 0;
+ }
+
+if (calc.bronzetrumpets.checked) {
+   var wbronzetrumpets = document.calc.bronzetrumpets.value = 2495;
+ } else {
+   var wbronzetrumpets = document.calc.bronzetrumpets.value = 0;
+ }
+
+
+if (calc.badges.checked) {
+   var wbadges = document.calc.badges.value = 495;
+ } else {
+   var wbadges = document.calc.badges.value = 0;
+ }
+
+if (calc.coldheat.checked) {
+   var wcoldheat = document.calc.coldheat.value = 7995;
+ } else {
+   var wcoldheat = document.calc.coldheat.value = 0;
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.calc.pay.value = 99995 + wfilefinish + wcopperbody + wbronzebody + wbrushedaluminum + wpabody + wpmstripes + wmeatball + wppstripes + wlemanstop  + w3steelframe + w3ssframe + wrollbar + wssrollbar + w2xrollbar + wrhd + wjackhooks + wbrakescoops + wheatshields + wnofoam +wfoam + winstfoam + wnyloncarpet +wnyloncarpetinst + wwoolcarpet + wwoolcarpetinst + wseattracks + wsunvisor + wwindwings + wpedals
+ + wpwdbulk + wwheel15  + wwheelleather + wwheel16 + wleatherdash + wseatbelt + wfireext + wfaird + wfairp + wdropbox + wharness + wbatteries + wbatteryswitch + wgauges + woilcooler + woilassy +
+ wwindsormount + wfuelfilter + wstainlesssteering + wsidesteel + wsidestainless + wtremecmount + walumdiff + wdiffadapt + wdiffhose + wdiffcooler + wdriveshaft + whalfshafts + wsafetyloop + wbilletsusp + wstandardshocks + wpenskeshocks + wpenskeadj + wstdbrakes + wgirling + wsslines + wres + wtires + wbleed + wsteelshaft + wfiacoolant + wbilletcoolant + waltbracket + woilvap + wtrspring + wekit + wenginefia + wetrans + wti + winstallengine + wbronzetpan + wglare + wbronzebling + wtrumpets + wbronzetrumpets + wbadges + wcoldheat;
+}
